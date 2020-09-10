1fcbbba446df4482d14dce9b5bf20ba3d172a5c9
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-09-10 16:38:29 -0400

    Redo Camera Script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraControllerTest : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset;
+
+    // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position;
